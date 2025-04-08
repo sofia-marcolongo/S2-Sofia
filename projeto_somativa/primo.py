4ffb54e68070464ef4ab466b110ce63931312fee
sofia-marcolongo <sofia.m.santos9@aluno.senai.br>
2025-04-08 09:09:41 -0300

    Exercício alterado
    Esse arquivo foi alterado.

--- projeto_somativa/primo.py
+++ projeto_somativa/primo.py
@@ -2,15 +2,14 @@
 
 print("olá mundo! Vamos analisar se um número é primo ou não!")
 
-
+num == 0 
 def primo(num1):
-        
+      print("Olá, mundo!") 
         for i in range(1, num1):
                 if num1 % i == 0:
-                        
+                   for i in range     
         if cont == 1:
                 print("o número é primo!")
-        
+        if
                 print("o número não é primo")
-
-
+print("bye bye mundo!")
